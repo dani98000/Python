a640f44062287ee42815ccdff6eedfc1e5110c78
daniel_maizel <dani98000k@gmail.com>
2019-05-16 11:43:27 +0300

    exercise 5 DONE

--- c/WS-5/Logger.c
+++ c/WS-5/Logger.c
@@ -3,123 +3,127 @@
 *	      Date: 15/5/19         *
 ********************************/
 
-#include <stdio.h>
-#include <string.h>
-#include <strings.h>
-#include <stdlib.h>
+#include <stdio.h> /*printf*/
+#include <string.h> /*strspn*/
+#include <strings.h> /*strcasecmp*/
+#include <stdlib.h> /*exit*/
 
 #define UNUSED(x) (void)(x)
-#define DATA_SIZE 1000
  	
-int AlwaysTrue(char UserInput[],char condition[]);
+int AlwaysTrue(char UserInput[], char condition[]);
 int Delete(char UserInput[], char *FileName);
 int Exit();
 int Count(char UserInput[], char *FileName);
 int CompFunc(char UserInput[], char condition[]);
 int DefaultAppend(char UserInput[], char *FileName);
 int BeginningOfFile(char *UserInput, char *FileName);
-int IsFirst(char UserInput[],char condition[]);
+int IsFirst(char UserInput[], char condition[]);
+const struct item *ChainInit();
 	
-
+enum ERROR_LIST {OK = 1 , ERROR, EXIT}; 
 
 struct item
 {
-    char condition[DATA_SIZE];
-	int (*cfp)(char UserInput[],char condition[]);
-	int (*ofp)(char UserInput[],char *FileName);
-} 
-Chain[5]= 
-{    
-   	{"-count\n", CompFunc, Count},
-    {"-remove", CompFunc, Delete},
-   	{"-exit\n", CompFunc, Exit},
-    {"i", AlwaysTrue, DefaultAppend},
-    {"<", IsFirst, BeginningOfFile}
+    char *condition;
+	int (*cfp)(char UserInput[], char condition[]);
+	int (*ofp)(char UserInput[], char *FileName);
 };
-
 
 int main(int argc, char *argv[])
 {
-
-	int i;
-	int flag =0;
+	int i = 0, d = 0;
+	int flag = 0;
 	int res = 0; 
-	int d;
 	char *FileName;
 	char UserInput[100];
+	const struct item *Chain = ChainInit();
 	FileName = argv[1];
 	UNUSED(argc);
 	UNUSED(d);
 	
-			
 	while(!flag)
 	{
-	fgets(UserInput, 8, stdin);
+		fgets(UserInput, 8, stdin);
 		
-		for(i = 0; i < 5; i++)
+		for(i = 0; i < 5; ++i)
 		{
 			if(Chain[i].cfp(UserInput, Chain[i].condition))
 			{	
-				d=Chain[i].cfp(UserInput, Chain[i].condition);
+				d = Chain[i].cfp(UserInput, Chain[i].condition);
 				res = Chain[i].ofp(UserInput, FileName);
-				if(res == 4)
+				if(res == EXIT)
 				{
 					flag = 1;
+					break;
+				}
+				if(res == ERROR)
+				{
+					printf("Error Occured");
 					break;
 				}
 				break;
 			}
 		}
-		i=0;
+		i = 0;
 	}
 	
 	return 0;
 }
 	
 
-int CompFunc(char UserInput[],char condition[])
+int CompFunc(char UserInput[], char condition[])
 {
-	return(strcasecmp(UserInput,condition) == 0);
+	return(0 == strcasecmp(UserInput,condition));
 } 
 
-int IsFirst(char UserInput[],char condition[])
+int IsFirst(char UserInput[], char condition[])
 {
-	return(strspn(UserInput,"<") == 1);
+	UNUSED(condition);
+	
+	return(strspn(UserInput, condition) == 1);
 } 
 
 
-int AlwaysTrue(char UserInput[],char condition[])
+int AlwaysTrue(char UserInput[], char condition[])
 {
 	UNUSED(UserInput);
 	UNUSED(condition);
-	return 1;
+	
+	return OK;
 }
 
 int Count(char *UserInput, char *FileName)
 {
-	int lines=0;
-	FILE *fPtr = fopen(FileName , "r+");
+	int lines = 0;
 	char ch;
+	FILE *fPtr = fopen(FileName , "r+");
 	UNUSED(UserInput);
-	while((ch=fgetc(fPtr))!=EOF)
+	
+	if(NULL == fPtr)
+    {
+		fPtr = fopen(FileName , "w+");
+    }
+ 
+	while((ch = fgetc(fPtr)) != EOF)
 	{
-	    if(ch == '\n')
+	    if('\n' == ch)
 	    {
 	    	++lines;
 	    }
 	}
 	
 
-    fprintf(fPtr,"%d\n",lines);
+    fprintf(fPtr, "%d\n", lines);
     fclose(fPtr);
+    
     return 0;
 }
 
 int Exit()
 {
-	printf("Exit");
-	exit(0);
-	return 4;
+	printf("Program Closed");
+	
+	return EXIT;
 }
 
 int Delete(char UserInput[], char *FileName)
@@ -127,17 +131,14 @@ int Delete(char UserInput[], char *FileName)
 	int ret = remove(FileName);
 	UNUSED(UserInput);
 
-    if(ret == 0) 
+    if(0 == ret) 
     {
     	printf("File deleted successfully\n");
-    	exit(0);
+    	
+    	return EXIT;
     }
-    else 
-    {
-  	  printf("Error: unable to delete the file\n");
-    }
-   
-	return(0);
+    
+	return 0;
 }
 
 int DefaultAppend(char *UserInput, char *FileName)
@@ -145,44 +146,52 @@ int DefaultAppend(char *UserInput, char *FileName)
 	FILE *fPtr = fopen(FileName, "a");
 	int i = 0;
 	
-	if(fPtr == NULL)
+	if(NULL == fPtr)
     {
         printf("Unable to create file.\n");
-        exit(0);
+        
+        return EXIT;
     }
     
-    while(UserInput[i] != '\0')
+    while('\0' != UserInput[i])
     {
 		putc(UserInput[i], fPtr);
 		++i;
 	}
-	
 	fclose(fPtr);
-	return 1;
+	
+	return OK;
 }
 
 int BeginningOfFile(char *UserInput, char *FileName)
 {
-	FILE *temp = fopen("temp.txt", "r+");
+	FILE *temp = fopen("temp.txt", "w+");
 	FILE *fPtr = fopen(FileName, "r+");
-	char c,d;
-	if(fPtr == NULL)
+	char c, d;
+	
+	if(NULL == fPtr)
+    {
+    	fPtr = fopen(FileName, "w+");
+    }
+    if(NULL == temp)
     {
         printf("Unable to create file.\n");
-        exit(0);
+        
+		return ERROR;
     }
 	c = fgetc(fPtr);
-	while (c != EOF) 
+	
+	while(c != EOF) 
     { 
         fputc(c, temp); 
         c = fgetc(fPtr); 
     }  
+    
    	rewind(fPtr);
    	rewind(temp);
-
-    fprintf(fPtr,"%s",UserInput);
-    
+    fprintf(fPtr, "%s", UserInput + 1);
 	d = fgetc(temp);
+	
 	while (d != EOF) 
     { 
         fputc(d, fPtr); 
@@ -192,5 +201,20 @@ int BeginningOfFile(char *UserInput, char *FileName)
 	fclose(fPtr);
 	fclose(temp);
 	remove("temp.txt");
-	return 1;
+	
+	return OK;
+}
+
+const struct item *ChainInit()
+{
+	static const struct item Chain[5]= 
+	{    
+   		{"-count\n", CompFunc, Count},
+    	{"-remove", CompFunc, Delete},
+   		{"-exit\n", CompFunc, Exit},
+    	{"<", IsFirst, BeginningOfFile},
+    	{" ", AlwaysTrue, DefaultAppend}
+	};
+	
+	return (Chain); 
 }
