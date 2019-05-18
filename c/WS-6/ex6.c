2a45bdebe6789c16e8c0bcd2c1d2fdd20d01c3b0
dani98000 <dani98000k@gmail.com>
2019-05-18 23:57:40 +0300

    continue at class

--- c/WS-6/ex6.c
+++ c/WS-6/ex6.c
@@ -56,7 +56,7 @@ int IsPow2(unsigned int n)
 	return 0;
 }
 
-unsigned int CountSet(unsigned int arr[], int arr_length)
+unsigned int ThreeSet(unsigned int arr[], int arr_length)
 {
 	int i, temp, count = 0;
 	for(i = 0; i < arr_length; i++)
@@ -97,18 +97,75 @@ void byte_mirror(unsigned int *n)
 	printf("%d\n",*n);
 }
 
-	
+int Div16(unsigned int a)
+{
+	if(a % 16 != 0)
+	{
+
+		a = a & 0xF0;
+	}
+	return a;
+}
+
+unsigned int CountSet(unsigned int num)
+{
+	int count = 0;
+	while(num >= 1) 
+	{	
+		if((num & 1) == 1)
+		{
+			++count;
+		}
+		num = num>>1;
+	}
+	return count;
+}
+
+int TwoAndSix(unsigned char a)
+{
+	return((a & 0x22) == 0x22);
+}
+
+int TwoOrSix(unsigned char a)
+{
+	return(((a & 0x02) == 0x02) || ((a & 0x20) == 0x20));
+}
+
+void swap(unsigned int x,unsigned int y)
+{
+	x = x ^ y; 
+    y = x ^ y; 
+    x = x ^ y;
+    printf("x = %d\ny = %d\n",x,y);
+}
+
+int SwapBits(unsigned int num) 
+{ 
+    unsigned int bit1 =  (num >> 2) & 1; 
+    unsigned int bit2 =  (num >> 4) & 1; 
+    unsigned int x = (bit1 ^ bit2); 
+    unsigned int result;
+    /* Put the xor bit back to their original positions */
+    x = (x << 2) | (x << 4); 
+	result = num ^ x; 
+    return result;
+} 
 
 int main()
 {
 	unsigned int a[] = {8, 5, 10, 12, 7, 14};
-	unsigned int b=1;
 	printf("%d\n", pow2(5, 2));
 	printf("%d\n", IsPow2(2));
 	printf("%d\n", IsPow2L(2));
 	Plusone(30);
-	CountSet(a, 6);
-	byte_mirror(&b);
+	ThreeSet(a, 6);
+	printf("%d\n",Div16(49));
+	printf("%d\n",CountSet(32));
+	printf("%d\n",TwoAndSix(32));
+	printf("%d\n",TwoOrSix(32));
+	swap(10,20);
+	printf("%d\n",SwapBits(14));
+	
 	return 0;
 }
 
