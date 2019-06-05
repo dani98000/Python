953898f4cb138da93fa5bf3e97812a025274502d
dani98000 <dani98000k@gmail.com>
2019-06-05 07:33:40 +0300

    Done some small changes on cbuf after code review

--- ds/lib/CBUF/cbuf2.c
+++ ds/lib/CBUF/cbuf2.c
@@ -1,8 +1,15 @@
+/********************************
+* 	 Author  : Daniel Maizel	*
+*	 Date    : 27/05/2019		*
+*	 Reviewer: Mishel          	*
+*								*
+*********************************/
+
 #include <stdlib.h> /* malloc */
 #include <assert.h> /* assert */
 #include <errno.h>  /* perror */
 
-#include "../../include/CBUF/cbuf.h"
+#include "../../include/CBUF/cbuf.h" /* header file */
 
 struct CBUF
 {
@@ -14,14 +21,20 @@ struct CBUF
 
 cbuf_t *CBUFCreate(size_t nbytes)
 {
-    cbuf_t *cbuf = (cbuf_t *)malloc(sizeof(cbuf_t));
+	cbuf_t *cbuf = NULL;
     
     assert(nbytes > 0);
     
+    if(0== nbytes)
+    {
+		return NULL;
+	}	
+    cbuf = (cbuf_t *)malloc(sizeof(cbuf_t));        
 	if(NULL == cbuf)
 	{
 		return NULL;
 	}
+	
 	
 	cbuf->buff = (void *)calloc(nbytes,sizeof(char));
 	if (NULL == cbuf->buff)
@@ -38,6 +51,8 @@ cbuf_t *CBUFCreate(size_t nbytes)
 
 void CBUFDestroy(cbuf_t *cbuf)
 {
+	assert(NULL != cbuf && NULL != cbuf->buff);
+	
 	free(cbuf->buff);
 	free(cbuf);
 }
@@ -47,8 +62,9 @@ size_t CBUFRead(cbuf_t *cbuf, void *data, size_t nbytes)
 	size_t count = 0;
 	
 	assert(NULL != cbuf);
+	assert(NULL != data);
 	
-    while (cbuf->size > 0  && nbytes > 0)
+    while (cbuf->size > 0 && nbytes > 0)
 	{
   		((char *)data)[count] = ((char*)cbuf->buff)[cbuf->read_offset];
    		 ++cbuf->read_offset;
@@ -98,14 +114,14 @@ int CBUFIsEmpty(const cbuf_t *cbuf)
 {
 	assert(NULL != cbuf);	
 
-	return(0 == cbuf->size);
+	return (0 == cbuf->size);
 }
 
 size_t CBUFFreeSpace(const cbuf_t *cbuf)
 {
 	assert(NULL != cbuf);
 
-	return(cbuf->capacity - cbuf->size);
+	return (cbuf->capacity - cbuf->size);
 }
 
 size_t CBUFCapacity(const cbuf_t *cbuf)
