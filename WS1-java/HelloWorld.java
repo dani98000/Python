febbffbfecf1a5f9257984bc07245c4f1853b4a9
root <dani98000k@gmail.com>
2019-08-24 23:41:02 +0300

    ws1 done

---
+++ WS1-java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello, World!");
+		MySecondClass daniel = new MySecondClass();
+		daniel.foo2();
+		MySecondClass.foo1();
+	}
+}
---
+++ WS1-java/MySecondClass.java
@@ -0,0 +1,10 @@
+public class MySecondClass{
+	static void foo1(){
+		System.out.println("This is foo1!");
+	}
+
+	void foo2(){
+		System.out.println("This is foo2!");
+	}
+
+}
