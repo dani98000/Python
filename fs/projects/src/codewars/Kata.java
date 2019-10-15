e62c23ed6276c807523ff898a12a0dea0c29737d
daniel <daniel98000k@gmail.com>
2019-10-15 18:48:17 +0300

    update

--- fs/projects/src/codewars/Kata.java
+++ fs/projects/src/codewars/Kata.java
@@ -1,29 +1,25 @@
 package codewars;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Stack;
 
 public class Kata {
-		public static int josephusSurvivor(final int n, final int k) {
-			HashMap<Integer, Integer> map = new LinkedHashMap<>();
-            if(map.containsKey(nums[i][j])){
+    public static boolean isAnagram(String s, String t) {
+        char[] a = s.toCharArray();
+        Arrays.sort(a);
+        char[] b = t.toCharArray();
+        Arrays.sort(b);
+        
+        System.out.println(a);
+        System.out.println(b);
+        
+        return Arrays.equals(a, b);
+    }
 
-			for(int i = 0; i < n; ++i) {
-				map.put(i, (i + k) % n);
-			}
-			
-			while(map.size() != 1) {
-				int i = 0;
-				map.remove(i);
-				map.replace(i, (i + k) % n);
-				i = (i + 3) % k;
-			}
-			
-			return map.entrySet().iterator().next().getKey();
-		}  
-
-		public static void main(String[] args) {
-			System.out.println(Kata.josephusSurvivor(7,3));
-		}
+	public static void main(String[] args) {
+		System.out.println(Kata.isAnagram("eat", "ate"));
+	}
 }
