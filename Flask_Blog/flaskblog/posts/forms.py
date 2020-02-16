fea92cc68446e290a9650adf70551a9d5278ab0f
daniel <dani98000k@gmail.com>
2020-02-16 10:46:22 +0200

    update

--- Flask_Blog/flaskblog/posts/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired(), Length(max=200)])
-    submit = SubmitField('Post')
